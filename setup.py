fe5f0d78025a6ccc3a3bad49096845fa8bd54a5e
Sean Xie <xieshengxiang@gmail.com>
2013-12-09 10:30:33 -0800

    remove __version__

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
-from referral_candy import __version__
 
 NAME='referral_candy'
 DESCRIPTION='ReferralCandy Python API Client'
 
 setup(name=NAME,
-      version=__version__,
+      version='0.1.0',
       description=DESCRIPTION,
       long_description=DESCRIPTION,
       author='ReferralCandy',
