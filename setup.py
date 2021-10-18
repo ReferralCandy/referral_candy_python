f217aa8d0d5364c99190818dbeaf10b1c5130688
Timothy Tan <timmoti@gmail.com>
2021-10-18 10:57:26 +0800

    Bump version. Compatible with python 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ NAME='referral_candy'
 DESCRIPTION='ReferralCandy Python API Client'
 
 setup(name=NAME,
-      version='0.1.0',
+      version='0.2.0',
       description=DESCRIPTION,
       long_description=DESCRIPTION,
       author='ReferralCandy',
